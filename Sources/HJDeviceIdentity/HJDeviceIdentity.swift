601af131a6c2bfa88be0c0d02bf74e147fc5f0f6
Han <tank2308635@163.com>
2020-10-29 19:21:47 +0800

    update

--- Sources/HJDeviceIdentity/HJDeviceIdentity.swift
+++ Sources/HJDeviceIdentity/HJDeviceIdentity.swift
@@ -1,3 +1,50 @@
+import KeychainAccess
+import AdSupport
+
+
+struct IdentifierConfig {
+    static let KIdentifier = "KIdentifier"
+
+    /// idfa 校验正则：都是0
+    static let InvalidIdfaRegex = #"^0+$"#
+}
+
 struct HJDeviceIdentity {
-    var text = "Hello, World!"
+
+    private static let keychain = Keychain()
+
+    static var identifier: String {
+        /// 获取keychain中的标识
+        if let id = getIdentifierInKeychain() {
+            return id
+        }
+
+        /// keychain中没有，就从idfa或uuid中获取一个有效的标识
+        let validId = getValidIdentifier()
+        /// 存储标识
+        saveIdentifierInKeychain(validId)
+        return validId
+    }
+
+    static func getValidIdentifier() -> String {
+        let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+        if ad_isValidFor(idfa: idfa) {
+            return idfa
+        }
+        return UUID().uuidString
+    }
+
+    static func ad_isValidFor(idfa: String) -> Bool {
+        let cleanIdfa = idfa.replacingOccurrences(of: "-", with: "")
+        let range = cleanIdfa.range(of: IdentifierConfig.InvalidIdfaRegex, options: .regularExpression, range: nil, locale: nil)
+        return range == nil
+    }
+
+    static func getIdentifierInKeychain() -> String? {
+        return keychain[IdentifierConfig.KIdentifier]
+    }
+
+    static func saveIdentifierInKeychain(_ id: String) {
+        try? keychain.set(id, key: IdentifierConfig.KIdentifier)
+    }
 }
